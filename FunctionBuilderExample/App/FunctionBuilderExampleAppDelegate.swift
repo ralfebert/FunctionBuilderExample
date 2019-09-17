b4774b43d292eea28a5214f8e8f905ce36cd4d81
Ralf Ebert <info@ralfebert.de>
2019-09-17 18:07:29 +0200

    Project setup FunctionBuilderExample

---
+++ FunctionBuilderExample/App/FunctionBuilderExampleAppDelegate.swift
@@ -0,0 +1,14 @@
+// © 2019 Ralf Ebert — FunctionBuilderExample
+
+import UIKit
+
+@UIApplicationMain
+class FunctionBuilderExampleAppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# FunctionBuilderExample
